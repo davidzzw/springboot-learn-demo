8d541850c54c6f8c1d24909e9aba38c4418ef37a
zhangzewei <zhangzewei@chezhibao.com>
2018-04-26 20:42:05 +0800

    新增sofa测试demo

---
+++ sofatest/src/main/java/com/zzw/sofatest/SofatestApplication.java
@@ -0,0 +1,12 @@
+package com.zzw.sofatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofatestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SofatestApplication.class, args);
+    }
+}
